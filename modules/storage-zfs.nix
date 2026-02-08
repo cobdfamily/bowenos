b0b82a57c1f6f5a64fecf275076639a5c530d78b
Alexandro Jurgensen <alexandrojurgensen@MacBookAir.local>
2026-02-08 14:50:39 -0800

    Fixing for 25.11

--- modules/storage-zfs.nix
+++ modules/storage-zfs.nix
@@ -5,8 +5,6 @@
   boot.zfs.devNodes = "/dev/disk/by-id";
   boot.zfs.extraPools = [ "rpool" ];
   boot.zfs.forceImportRoot = true;
-  boot.initrd.systemd.enable = true;
-  boot.initrd.systemd.extraUnits."zfs-import.service".wantedBy = [ "initrd.target" ];
   services.zfs.autoScrub.enable = true;
   services.zfs.trim.enable = true;
 
