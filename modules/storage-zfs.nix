03b71db32cf0ece1338f4b51a1498852df839461
Alexandro Jurgensen <alexandrojurgensen@MacBookAir.local>
2026-02-08 13:47:25 -0800

    Import rpool during initrd

--- modules/storage-zfs.nix
+++ modules/storage-zfs.nix
@@ -3,6 +3,7 @@
   boot.supportedFilesystems = [ "zfs" ];
   boot.initrd.supportedFilesystems = [ "zfs" ];
   boot.zfs.devNodes = "/dev/disk/by-id";
+  boot.zfs.extraPools = [ "rpool" ];
   boot.zfs.forceImportRoot = true;
   services.zfs.autoScrub.enable = true;
   services.zfs.trim.enable = true;
