7fc1a7af8283a8a562dd8722db5ad4aad2c901df
Alexandro Jurgensen <alexandrojurgensen@MacBookAir.local>
2026-02-08 10:51:39 -0800

    Ensure ZFS is in initrd

--- modules/zfs.nix
+++ modules/zfs.nix
@@ -1,6 +1,7 @@
 { ... }:
 {
   boot.supportedFilesystems = [ "zfs" ];
+  boot.initrd.supportedFilesystems = [ "zfs" ];
   boot.zfs.devNodes = "/dev/disk/by-id";
   services.zfs.autoScrub.enable = true;
   services.zfs.trim.enable = true;
