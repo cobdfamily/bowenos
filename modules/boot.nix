c1a72eb84cf6626850894c1bfb74a00570a63a62
Alexandro Jurgensen <alexandrojurgensen@MacBookAir.local>
2026-02-08 13:25:02 -0800

    Fix boot module assertions placement

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, config, ... }:
 let
   get = name: def: let v = builtins.getEnv name; in if v == "" then def else v;
   mkPath = x: if lib.hasPrefix "/dev/" x then x else "/dev/disk/by-id/" + x;
@@ -8,29 +8,30 @@ let
   bootaPath = mkPath boota;
   bootbPath = mkPath bootb;
 
-  diskMode = get "DISK_MODE" "mirror";
+  diskMode = lib.attrByPath [ "bowenos" "storage" "diskMode" ] "mirror" config;
   useMirror = diskMode == "mirror";
 
   bootMode = get "BOOT_MODE" "uefi";
   useEfi = bootMode == "uefi";
 in
 {
-  assertions = [
-    {
-      assertion = (bootMode == "uefi") || (bootMode == "bios");
-      message = "BOOT_MODE must be 'uefi' or 'bios'.";
-    }
-    {
-      assertion = (bootMode != "bios") || (boota != "");
-      message = "BOOTA_BYID is required when BOOT_MODE=bios.";
-    }
-    {
-      assertion = (!useMirror) || (bootb != "");
-      message = "BOOTB_BYID is required when DISK_MODE=mirror.";
-    }
-  ];
-
   config = lib.mkMerge [
+    {
+      assertions = [
+        {
+          assertion = (bootMode == "uefi") || (bootMode == "bios");
+          message = "BOOT_MODE must be 'uefi' or 'bios'.";
+        }
+        {
+          assertion = (bootMode != "bios") || (boota != "");
+          message = "BOOTA_BYID is required when BOOT_MODE=bios.";
+        }
+        {
+          assertion = (!useMirror) || (bootb != "");
+          message = "BOOTB_BYID is required when DISK_MODE=mirror.";
+        }
+      ];
+    }
     (lib.mkIf useEfi {
       # Ensure EFI boot is configured.
       boot.loader.systemd-boot.enable = true;
