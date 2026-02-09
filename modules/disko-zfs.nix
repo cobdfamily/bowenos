9d570b37e65f0478b0e3fcc758babd894f57c4f5
Alexandro Jurgensen <alexandrojurgensen@MacBookAir.local>
2026-02-08 19:02:12 -0800

    Set ZFS bootfs to rpool/root

--- modules/disko-zfs.nix
+++ modules/disko-zfs.nix
@@ -84,7 +84,7 @@ in
     zpool.rpool = {
       type = "zpool";
       mode = if useMirror then "mirror" else "single";
-      options = { ashift = "12"; autotrim = "on"; };
+      options = { ashift = "12"; autotrim = "on"; bootfs = "rpool/root"; };
 
       rootFsOptions = {
         compression = "zstd";
