233801a820c54c08078cd7a920ec4125f18afeca
Alexandro Jurgensen <alexandrojurgensen@MacBookAir.local>
2026-02-08 21:35:24 -0800

    Delay initrd device discovery

--- modules/storage-zfs.nix
+++ modules/storage-zfs.nix
@@ -2,6 +2,10 @@
 {
   boot.supportedFilesystems = [ "zfs" ];
   boot.initrd.supportedFilesystems = [ "zfs" ];
+  boot.initrd.preDeviceCommands = ''
+    udevadm settle --timeout=10 || true
+    sleep 2
+  '';
   boot.zfs.devNodes = if config.bowenos.storage.isVm then "/dev/disk/by-path" else "/dev/disk/by-id";
   boot.zfs.extraPools = [ "rpool" ];
   boot.zfs.forceImportRoot = true;
