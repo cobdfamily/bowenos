0da5b383cbc35eaecc20bd8921d7715abe70e0c7
Alexandro Jurgensen <alexandrojurgensen@Chocolate-Lab.local>
2026-02-10 14:42:18 -0800

    zfs: move preDeviceCommands to initrd systemd

--- modules/storage-zfs.nix
+++ modules/storage-zfs.nix
@@ -1,11 +1,23 @@
-{ config, ... }:
+{ lib, config, pkgs, ... }:
 {
   boot.supportedFilesystems = [ "zfs" ];
   boot.initrd.supportedFilesystems = [ "zfs" ];
-  boot.initrd.preDeviceCommands = ''
+  boot.initrd.preDeviceCommands = lib.mkIf (!config.boot.initrd.systemd.enable) ''
     udevadm settle --timeout=10 || true
     sleep 2
   '';
+  boot.initrd.systemd.services.zfs-udev-settle = lib.mkIf config.boot.initrd.systemd.enable {
+    description = "Wait for udev and disks before ZFS import";
+    wantedBy = [ "initrd.target" ];
+    before = [ "zfs-import.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = [
+        "${pkgs.systemd}/bin/udevadm settle --timeout=10"
+        "${pkgs.coreutils}/bin/sleep 2"
+      ];
+    };
+  };
   boot.zfs.devNodes = if config.bowenos.storage.isVm then "/dev/disk/by-path" else "/dev/disk/by-id";
   boot.zfs.extraPools = [ "rpool" ];
   boot.zfs.forceImportRoot = true;
