4f40515ba3c300d1595369578f8f40b5b1573c06
Alexandro Jurgensen <alexandrojurgensen@Chocolate-Lab.local>
2026-02-12 21:49:40 -0800

    Make bcrail input optional to avoid module-arg recursion

--- modules/services/bcrail.nix
+++ modules/services/bcrail.nix
@@ -1,67 +1,53 @@
-{ lib, pkgs, config, inputs, ... }:
+args@{ lib, pkgs, config, ... }:
 let
   cfg = config.bowenos.bcrail;
-  bcrail = inputs.bcrail;
+
+  hasInputs = args ? inputs;
+  inputs = if hasInputs then args.inputs else { };
+  hasBcrailInput = hasInputs && (inputs ? bcrail);
+  bcrail = if hasBcrailInput then inputs.bcrail else null;
 in
 {
-  imports = [ bcrail.nixosModules.default ];
+  imports = lib.optionals hasBcrailInput [ bcrail.nixosModules.default ];
 
   options.bowenos.bcrail = {
     enable = lib.mkEnableOption "bcrail integration";
 
-    stateDir = lib.mkOption {
-      type = lib.types.str;
-      default = "/var/lib/bcrail";
-    };
-
-    configDir = lib.mkOption {
-      type = lib.types.str;
-      default = "/etc/bcrail";
-    };
-
-    bridge = lib.mkOption {
-      type = lib.types.str;
-      default = "incusbr0";
-    };
-
-    pool = lib.mkOption {
-      type = lib.types.str;
-      default = "zfs-ssd";
-    };
-
-    remoteUser = lib.mkOption {
-      type = lib.types.str;
-      default = "vancouver";
-    };
-
-    stateDevice = lib.mkOption {
-      type = lib.types.nullOr lib.types.str;
-      default = null;
-    };
-
-    setupOnBoot = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-    };
+    stateDir = lib.mkOption { type = lib.types.str; default = "/var/lib/bcrail"; };
+    configDir = lib.mkOption { type = lib.types.str; default = "/etc/bcrail"; };
+    bridge = lib.mkOption { type = lib.types.str; default = "incusbr0"; };
+    pool = lib.mkOption { type = lib.types.str; default = "zfs-ssd"; };
+    remoteUser = lib.mkOption { type = lib.types.str; default = "vancouver"; };
+    stateDevice = lib.mkOption { type = lib.types.nullOr lib.types.str; default = null; };
+    setupOnBoot = lib.mkOption { type = lib.types.bool; default = false; };
   };
 
-  config = {
-    bowenos.bcrail.enable = lib.mkDefault true;
+  config = lib.mkMerge [
+    { bowenos.bcrail.enable = lib.mkDefault hasBcrailInput; }
+    {
+      assertions = [
+        {
+          assertion = (!cfg.enable) || hasBcrailInput;
+          message = "bowenos.bcrail.enable requires flake input 'bcrail' and passing 'inputs' in specialArgs.";
+        }
+      ];
+    }
+    (if (cfg.enable && hasBcrailInput) then {
+      services.bcrail = {
+        enable = true;
+        package = bcrail.packages.${pkgs.system}.bcrail;
 
-    services.bcrail = lib.mkIf cfg.enable {
-      enable = true;
-      package = bcrail.packages.${pkgs.system}.bcrail;
+        stateDir = cfg.stateDir;
+        configDir = cfg.configDir;
+        network.bridge = cfg.bridge;
+        storage.pool = cfg.pool;
+        remoteUser = cfg.remoteUser;
+        stateDevice = cfg.stateDevice;
+        setupOnBoot = cfg.setupOnBoot;
 
-      stateDir = cfg.stateDir;
-      configDir = cfg.configDir;
-      network.bridge = cfg.bridge;
-      storage.pool = cfg.pool;
-      remoteUser = cfg.remoteUser;
-      stateDevice = cfg.stateDevice;
-      setupOnBoot = cfg.setupOnBoot;
-
-      ignitionFile = bcrail + "/etc/bcrail/ignition.json";
-      locomotiveEnvFile = bcrail + "/etc/bcrail/locomotive.env";
-    };
-  };
+        ignitionFile = bcrail + "/etc/bcrail/ignition.json";
+        locomotiveEnvFile = bcrail + "/etc/bcrail/locomotive.env";
+      };
+    } else { })
+  ];
 }
