6c5c61953f059555c425bdbba215ad68a65db701
Alexandro Jurgensen <alexandrojurgensen@Chocolate-Lab.local>
2026-02-12 23:50:40 -0800

    Set additional BCRail options

--- modules/services/bcrail.nix
+++ modules/services/bcrail.nix
@@ -4,5 +4,18 @@
     ./bcrail-import.nix
   ];
 
-  services.bcrail.enable = true;
+  services.bcrail = {
+    enable = true;
+    # package = pkgs.bcrail; # default
+    stateDir = "/var/lib/bcrail";
+    configDir = "/etc/bcrail";
+
+    network.bridge = "incusbr0";
+    storage.pool = "zfs-ssd;
+
+    remoteUser = "vancouver";
+    stateDevice = "/dev/sdb"; # or e.g. "/dev/vdb"
+
+    setupOnBoot = false;
+  };
 }
