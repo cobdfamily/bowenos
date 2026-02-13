c262bfef2539538a45a72dfb9717760b5b2cf8a5
Alexandro Jurgensen <alexandrojurgensen@Chocolate-Lab.local>
2026-02-13 07:32:42 -0800

    Guard bowenos-tools option assignment behind option existence

--- modules/bowenos-tools.nix
+++ modules/bowenos-tools.nix
@@ -1,10 +1,15 @@
-{ ... }:
+{ lib, options, ... }:
+let
+  hasBowenosToolsOption = lib.hasAttrByPath [ "services" "bowenos-tools" "enable" ] options;
+in
 {
   imports = [
     ./imports/bowenos-tools-import.nix
   ];
 
-  services.bowenos-tools = {
-    enable = true;
+  config = lib.mkIf hasBowenosToolsOption {
+    services.bowenos-tools = {
+      enable = true;
+    };
   };
 }
