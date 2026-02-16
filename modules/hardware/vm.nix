cc04b43c42e833777d41456c3a55c39ac25650dc
Alexandro Jurgensen <alexandrojurgensen@Chocolate-Lab.local>
2026-02-15 20:01:58 -0800

    Made all nterfaces use DCP for the VMs

--- modules/hardware/vm.nix
+++ modules/hardware/vm.nix
@@ -31,7 +31,7 @@
   systemd.network = {
     enable = true;
     networks."50-enp5s0" = {
-      matchConfig.Name = "enp5s0";
+      matchConfig.Name = "*";
       networkConfig = {
         DHCP = "ipv4";
         IPv6AcceptRA = true;
