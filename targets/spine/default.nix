623eed8fae18208919cb82763267dff5a436b6b4
Alexandro Jurgensen <alexandrojurgensen@Chocolate-Lab.local>
2026-02-15 17:03:53 -0800

    Use DHCP on all ethernet links for spine

--- targets/spine/default.nix
+++ targets/spine/default.nix
@@ -8,13 +8,10 @@
 
   bowenos.storage.diskMode = lib.mkDefault "single";
 
-  # Spine does not use lan-bridge; request DHCP directly on common NIC names.
-  systemd.network.networks."10-spine-en-dhcp" = {
-    matchConfig.Name = "en*";
-    networkConfig.DHCP = "yes";
-  };
-  systemd.network.networks."10-spine-eth-dhcp" = {
-    matchConfig.Name = "eth*";
+  # Spine does not use lan-bridge; request DHCP on all physical Ethernet links.
+  networking.useDHCP = lib.mkForce true;
+  systemd.network.networks."10-spine-uplink-dhcp" = {
+    matchConfig.Type = "ether";
     networkConfig.DHCP = "yes";
   };
   systemd.network.wait-online.enable = lib.mkForce false;
