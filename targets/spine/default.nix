56a75e4bf673fd2c8f15d45bddd1c1aa516d5715
Alexandro Jurgensen <alexandrojurgensen@Chocolate-Lab.local>
2026-02-15 16:33:05 -0800

    Enable DHCP on spine network interfaces

--- targets/spine/default.nix
+++ targets/spine/default.nix
@@ -8,5 +8,15 @@
 
   bowenos.storage.diskMode = lib.mkDefault "single";
 
+  # Spine does not use lan-bridge; request DHCP directly on common NIC names.
+  systemd.network.networks."10-spine-en-dhcp" = {
+    matchConfig.Name = "en*";
+    networkConfig.DHCP = "yes";
+  };
+  systemd.network.networks."10-spine-eth-dhcp" = {
+    matchConfig.Name = "eth*";
+    networkConfig.DHCP = "yes";
+  };
+
   system.stateVersion = "25.11";
 }
