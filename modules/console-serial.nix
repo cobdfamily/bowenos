c3952c6bfa54ad13e89426192ce4f033ac83adf3
Alexandro Jurgensen <alexandrojurgensen@MacBookAir.local>
2026-04-15 11:15:23 -0700

    Fixed grb to TTYS1

--- modules/console-serial.nix
+++ modules/console-serial.nix
@@ -1,15 +1,15 @@
 { ... }:
 {
   boot.loader.grub.extraConfig = ''
-    serial --unit=0 --speed=115200 --word=8 --parity=no --stop=1
+    serial --unit=1 --speed=115200 --word=8 --parity=no --stop=1
     terminal_input serial
     terminal_output serial
   '';
 
   boot.kernelParams = [
     "console=tty0"
-    "console=ttyS1,115200n8"
     "console=ttyS0,115200n8"
+    "console=ttyS1,115200n8"
   ];
 
 }
